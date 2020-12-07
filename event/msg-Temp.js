65af43154a23536d82cb7c9c5667914565e0845d
Rizqy Prastya Ari Nugroho <rizqynugroho9@gmail.com>
2020-12-07 22:05:33 +0700

    FIx Package

--- event/msg-Temp.js
+++ event/msg-Temp.js
@@ -4,14 +4,14 @@ const {waFeed,waProblem} = require("./../setting/data/bot-setting.json")
 const infoProblem = `Bila terjadi kesalahan silahkan hubungi : ${waProblem} as R-Dev`
 const infoFeedback = `Untuk feedback dan selengkapya tentang Bot ini, bisa hubungi : ${waFeed} as *R-Dev*`
 
-// !For Global
+//! Message error
 
-// !For personal
 
-// !For Group
+
 
 
 module.exports ={
     infoProblem,
-    infoFeedback
+    infoFeedback,
+    error
 }
---
+++ test/parent.js
@@ -0,0 +1,4 @@
+const temp = require('./../event/msg-Temp')
+
+const a = temp.error[0]
+console.log(a);
