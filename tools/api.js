4b6da85a422a8a083e00dccd45f5e1cb9d032081
Rizqy Prastya Ari Nugroho <rizqynugroho9@Rizqys-MacBook-Pro.local>
2021-03-23 19:23:21 +0700

    backup

--- tools/api.js
+++ tools/api.js
@@ -1,19 +1,5 @@
 const axios = require("axios");
 
-// // IG Highlight
-// const ig_highlight = async (user,hl,hl_select) => new Promise((resolve,reject) => {
-//     axios.get(`http://arugaz.my.id/api/media/highlightig?user=${user}`)
-//     .then(res => {
-//         console.log(res)
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
-// })
-// // ig_highlight('rizqy.pan',1,1)
-
-
-// IG  Download
 
 // ! Tiktok Downloader
 const tiktok = async (link) => new Promise((resolve, reject) => {
@@ -25,6 +11,11 @@ const tiktok = async (link) => new Promise((resolve, reject) => {
         reject(err)
     })
 })
+
+
+
+
+
 
 module.exports = {
     // ig_highlight,
